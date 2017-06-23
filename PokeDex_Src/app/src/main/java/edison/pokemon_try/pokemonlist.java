33faedeff167f7961b99254eb240be3c98671c58
jokerrick <2548243695@qq.com>
2017-06-24 02:26:07 +0800

    项目文件

---
+++ PokeDex_Src/app/src/main/java/edison/pokemon_try/pokemonlist.java
@@ -0,0 +1,72 @@
+package edison.pokemon_try;
+
+/**
+ * Created by MSI on 2016/12/15.
+ */
+public class pokemonlist {
+    public static final String TABLE_NAME = "Edison_table";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_NUMBER = "number";
+    public static final String KEY_PHOTO = "photo";
+    public static final String KEY_PIXELPHOTO = "pixelphoto";
+    public static final String KEY_CHNAME = "chname";
+    public static final String KEY_JPNAME = "jpname";
+    public static final String KEY_ENNAME = "enname";
+    public static final String KEY_ATTRIBUTE1 = "attribute1";
+    public static final String KEY_ATTRIBUTE2 = "attribute2";
+    public static final String KEY_INTRODUCE = "introduce";
+    public static final String KEY_CLASSES = "classes";
+    public static final String KEY_CHARACTERISTIC1 = "characteristic1";
+    public static final String KEY_CHARACTERISTIC2 = "characteristic2";
+    public static final String KEY_HEIGHT = "height";
+    public static final String KEY_WEIGHT = "weight";
+    public static final String KEY_EGG1 = "egg1";
+    public static final String KEY_EGG2 = "egg2";
+    public static final String KEY_PROBABILITY = "probability";
+    public static final String KEY_STEPS = "steps";
+    public static final String KEY_HP = "hp";
+    public static final String KEY_HPSHOW = "hpshow";
+    public static final String KEY_WG = "wg";
+    public static final String KEY_WGSHOW = "wgshow";
+    public static final String KEY_WF = "wf";
+    public static final String KEY_WFSHOW = "wfshow";
+    public static final String KEY_TG = "tg";
+    public static final String KEY_TGSHOW = "tgshow";
+    public static final String KEY_TF = "tf";
+    public static final String KEY_TFSHOW = "tfshow";
+    public static final String KEY_SD = "sd";
+    public static final String KEY_SDSHOW = "sdshow";
+
+    public int id;
+    public String number;
+    public String photo;
+    public String pixelphoto;
+    public String chname;
+    public String jpname;
+    public String enname;
+    public String attribute1;
+    public String attribute2;
+    public String introduce;
+    public String classes;
+    public String characteristic1;
+    public String characteristic2;
+    public String height;
+    public String weight;
+    public String egg1;
+    public String egg2;
+    public String probability;
+    public String steps;
+    public String hp;
+    public String hpshow;
+    public String wg;
+    public String wgshow;
+    public String wf;
+    public String wfshow;
+    public String tg;
+    public String tgshow;
+    public String tf;
+    public String tfshow;
+    public String sd;
+    public String sdshow;
+}
